61dfe973aa94399d316ad5b4344f4ade31350675
Riazantsev <riazantsevmark327@gmail.com>
2023-09-23 09:12:49 +0300

    Done

---
+++ src/main/java/NotRegisteredException.java
@@ -0,0 +1,6 @@
+public class NotRegisteredException extends RuntimeException {
+
+    public NotRegisteredException(String playerName) {
+        super("Player with name " + playerName + " not found");
+    }
+}
